fc315c6f07a9f8ca11414a19d276396a60c35440
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-02-27 20:12:51 -0300

    Creating function to read lines from constructor

--- main.c
+++ main.c
@@ -5,9 +5,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "main.h"
+#include <ctype.h>
 
 #define MAXFILENAME 100
 #define MAXFILEBUFFER 1200
+#define MAXCONSTRUCTORLINES 20
+#define MAXCONSTRUCTORSIZELINES 100
 
 
 void readfilename(char * filename, char * varname, char * varfilecontent);
@@ -15,10 +18,15 @@ void strcatwithspace(char *dest, char *src);
 void execCommand(char * command, char *dest);
 void buildCommand(char *args, char *dest);
 void superstrcat(char *dest, char *argv[], int n);
+void getcontructorlines(char * filecontent, char *dest[]);
+void allocarrayofpointer(char *arg[]);
 
 int main(int argc, char *argv[]) {
   char *filename = (char * )malloc(MAXFILENAME);
   char *filecontent = (char * )malloc(MAXFILEBUFFER);
+  char *constructorlines[MAXCONSTRUCTORLINES];
+
+  allocarrayofpointer(constructorlines);
 
   if (argc < 2) {
     fprintf(stderr, "At least one arguments has to be provided");
@@ -33,6 +41,11 @@ int main(int argc, char *argv[]) {
   }
 
   printf("\n\nFILE CONTENT: %s\n\n", filecontent);
+  getcontructorlines(filecontent, constructorlines);
+  
+  for (int i = 0; i < 6; i++) {
+    printf("\n Constructor lines: %s", constructorlines[i]);
+  }
 
   return 0;
 }
@@ -53,7 +66,43 @@ void readfilename(char * filename, char * globalfilename, char *filecontent) {
   read(fd, filecontent, MAXFILEBUFFER);
 }
 
+void getcontructorlines(char * filecontent, char *dest[]) {
+  char constructor[] = "constructor";
+  char lineConstructor[400];
+  int countConstructor = 0;
 
+  for (int i = 0; i < strlen(filecontent); i++) {  
+    if (filecontent[i] == constructor[countConstructor++]) 
+      ;
+    else 
+      countConstructor = 0;
+
+    if (countConstructor == strlen(constructor)) {
+      int j = 0;
+      while(filecontent[++i] != '{') 
+        lineConstructor[j++] = filecontent[i];
+      
+      lineConstructor[j] = '\0';
+      break;
+    }
+  }
+
+  int destCounter = 0;
+  for (int i = 0; i < strlen(lineConstructor); i++) { 
+    if (lineConstructor[i] != '\0' && lineConstructor[i] != ' '){
+      int k = 0;
+      while (lineConstructor[i] != '\n' && i < strlen(lineConstructor)){
+        dest[destCounter][k++] = lineConstructor[i++];
+      }
+      if (i > 1) {
+        dest[destCounter][k] = '\0';
+        destCounter++;
+        k = 0;
+      }
+      
+      }
+  }
+}
 
 void buildCommand(char *args, char *dest) {
   char baseCommand[150] = "/bin/find ";
@@ -62,8 +111,6 @@ void buildCommand(char *args, char *dest) {
   char *superstrcatargv[] = { BASE_PATH, pipe, endGrepCommand, args};  
 
   superstrcat(baseCommand, superstrcatargv, 4);
-
-  printf("\nCOMMAND: %s\n", baseCommand);
 
   strcpy(dest, baseCommand);
 };
@@ -99,4 +146,10 @@ void strcatwithspace(char *dest, char *s) {
     }
     *dest++ = ' ';
     *dest = '\0';
+}
+
+void allocarrayofpointer(char *arg[]) {
+  for (int i = 0; i < MAXCONSTRUCTORLINES; i++) {
+    arg[i] = (char *) malloc(MAXCONSTRUCTORSIZELINES);
+  }
 }
