5173749023c1ed12ef76b8fb326ca8bf1a42dab8
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-02-27 22:49:43 -0300

    Creating function to assing variables and get variable names

--- main.c
+++ main.c
@@ -11,21 +11,32 @@
 #define MAXFILEBUFFER 1200
 #define MAXCONSTRUCTORLINES 20
 #define MAXCONSTRUCTORSIZELINES 100
+#define MAXVAR 10
 
+typedef struct {
+  char *type;
+  char *name;
+} Variables;
 
 void readfilename(char * filename, char * varname, char * varfilecontent);
 void strcatwithspace(char *dest, char *src);
-void execCommand(char * command, char *dest);
+void execCommand(char *command, char *dest);
 void buildCommand(char *args, char *dest);
 void superstrcat(char *dest, char *argv[], int n);
-void getcontructorlines(char * filecontent, char *dest[]);
+void getcontructorlines(char *filecontent, char *dest[]);
 void allocarrayofpointer(char *arg[]);
+void assingvariables(char *constructorlines[], Variables* destVar[]);
+void getvariablename(char *variableLine, char *destname);
+void allocarrayofstrucvar(Variables *argv[]);
+
 
 int main(int argc, char *argv[]) {
   char *filename = (char * )malloc(MAXFILENAME);
   char *filecontent = (char * )malloc(MAXFILEBUFFER);
   char *constructorlines[MAXCONSTRUCTORLINES];
+  Variables *vars[MAXVAR];
 
+  allocarrayofstrucvar(vars);
   allocarrayofpointer(constructorlines);
 
   if (argc < 2) {
@@ -46,6 +57,10 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < 6; i++) {
     printf("\n Constructor lines: %s", constructorlines[i]);
   }
+
+  assingvariables(constructorlines, vars);
+
+  printf("\n\n Var type: %s, Var name: %s", (*vars) -> type, (*vars) -> name);
 
   printf("\n\n\n");
 
@@ -105,6 +120,47 @@ void getcontructorlines(char * filecontent, char *dest[]) {
   }
 }
 
+void assingvariables(char *constructorlines[], Variables* destVar[]) {
+  char *name = (char *)malloc(MAXCONSTRUCTORSIZELINES);
+  for (int i = 0; i < MAXCONSTRUCTORLINES && constructorlines[i]; i++){
+    if (strstr(constructorlines[i], "Repository")){
+      getvariablename(constructorlines[++i], name);
+      (*destVar) -> type = "repository"; 
+      (*destVar) -> name = name;
+    }
+    else if (strstr(constructorlines[i], "UseCase")) {
+      getvariablename(constructorlines[++i], name);
+      (*destVar) -> type = "service"; 
+      (*destVar) -> name = name;
+    }
+
+    else if (strstr(constructorlines[i], "Helper")) {
+      getvariablename(constructorlines[++i], name);
+      (*destVar) -> type = "helper"; 
+      (*destVar) -> name = name;
+    }
+  }
+}
+
+void getvariablename(char *variableLine, char *destname) {
+  char limit[] = "readonly";
+  int limitCount = 0;
+ 
+  while(*variableLine != '\0') {
+     printf("\nLine here %c \n\n", *variableLine);
+    if (*variableLine++ == limit[limitCount]) {
+      limitCount++;
+    }
+
+    if (limitCount == strlen(limit)) {
+      while(*variableLine != ':') 
+        *destname++ = ++*variableLine;
+      
+      *destname = '\0';
+    }
+  }
+}
+
 void buildCommand(char *args, char *dest) {
   char baseCommand[150] = "/bin/find ";
   char pipe[] = "|";
@@ -150,7 +206,11 @@ void strcatwithspace(char *dest, char *s) {
 }
 
 void allocarrayofpointer(char *arg[]) {
-  for (int i = 0; i < MAXCONSTRUCTORLINES; i++) {
+  for (int i = 0; i < MAXCONSTRUCTORLINES; i++) 
     arg[i] = (char *) malloc(MAXCONSTRUCTORSIZELINES);
-  }
+}
+
+void allocarrayofstrucvar(Variables *arg[]){
+   for (int i = 0; i < MAXVAR; i++)
+    arg[i] = malloc(sizeof(Variables *));
 }
